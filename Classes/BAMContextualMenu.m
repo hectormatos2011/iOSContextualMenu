4fe61f00cf2b62314ce51ff6fe6e8cbf6d6db264
nathan chang <nathan@positivegrid.com>
2016-05-09 13:39:56 +0800

    Fix custom title view not working problem
    the titleView is overwrite by else case

--- Classes/BAMContextualMenu.m
+++ Classes/BAMContextualMenu.m
@@ -715,7 +715,7 @@
                 highlightedMenuItem = menuItem;
             }
             
-            UIView *titleView;
+            UIView *titleView = nil;
             
             if ([self.delegate respondsToSelector:@selector(contextualMenu:titleViewForMenuItemAtIndex:)]) {
                 titleView = [self.delegate contextualMenu:self titleViewForMenuItemAtIndex:index];
@@ -753,7 +753,7 @@
                 }
                 
                 titleView = titleLabel;
-            } else {
+            } else if(titleView == nil){
                 titleView = [[UIView alloc] initWithFrame:CGRectMake(0.0, 0.0, biggestMenuItemWidthHeight, 1.0)];
                 titleView.backgroundColor = [UIColor clearColor];
             }
